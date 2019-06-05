2106472fbc37fa1c8da8eef71e55f146f7fd1802
humichen <43415952+humichen@users.noreply.github.com>
2019-06-05 12:04:49 +0800

    Add files via upload

--- toneMelody/toneMelody.ino
+++ toneMelody/toneMelody.ino
@@ -17,6 +17,10 @@
 
 #include "pitches.h"
 int btnpin=2;
+int tonePin=8;
+int latchPin = 11;
+int clockPin = 7;
+int dataPin = 12;
 int btnstate=0;
 int oldbtn=0;
 
@@ -60,14 +64,14 @@ void loop() {
       // to calculate the note duration, take one second divided by the note type.
       //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
       int noteDuration = 1000 / noteDurations2[thisNote];
-      tone(8, melody2[thisNote], noteDuration);
+      tone(tonePin, melody2[thisNote], noteDuration);
 
       // to distinguish the notes, set a minimum time between them.
       // the note's duration + 30% seems to work well:
       int pauseBetweenNotes = noteDuration * 1.30;
       delay(pauseBetweenNotes);
       // stop the tone playing:
-      noTone(8);
+      noTone(tonePin);
     }
   }
   oldbtn=btnstate;
